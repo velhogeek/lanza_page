bc78518b00e6b0c380ca53194d076de964497e70
velhogeek <alexandre.lanza@gmail.com>
2021-01-09 21:32:52 -0300

    Simples

--- pages/index.js
+++ pages/index.js
@@ -44,19 +44,7 @@ export default function Home() {
       </main>
 
       <footer className={styles.footer}>
-        <p>Devo agradecer</p>
-        <br></br>
-        <ul>
-          <li><a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a></li>
-        <li><a href="https://www.youtube.com/channel/UCU5JicSrEM5A63jkJ2QvGYw"><img src="https://yt3.ggpht.com/ytc/AAUvwnjmOwHGXldA8NPXJ4vIFtA4334qT5fspJMPoM4p=s88-c-k-c0x00ffffff-no-rj"></img></a>Filipe Deschampse seus vídeos</li>
-        </ul>        
+        <p>PS: Ainda atualizando, me desculpe!</p>     
       </footer>
     </div>
   )

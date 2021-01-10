cba56978b222be5b8731943626ab9797ee0d3030
velhogeek <alexandre.lanza@gmail.com>
2021-01-10 20:30:52 -0300

    primeira versão 01/21

--- pages/index.js
+++ pages/index.js
@@ -21,30 +21,50 @@ export default function Home() {
         <div className={styles.grid}>
           <a href="#" className={styles.card}>
             <h3>Interesses &rarr;</h3>
-            <p>...</p>
+            <p>Pós Doutorado na área de materiais</p>
+            <p>Professor universitário área de exatas ou programação</p>
+            <p>Indústria, área de materiais, desenvolvimento de produtos</p>
           </a>
 
           <a href="#" className={styles.card}>
             <h3>Habilidades &rarr;</h3>
-            <p>...</p>
+            <ul>
+              <li>Flexibilidade</li>
+              <li>Autoconfiança e autoconhecimento</li>
+              <li>Iniciativa</li>
+              <li>Competitividade</li>
+              <li>Visão no cliente</li>
+              <li>Compreensão interpessoal e empatia</li>
+              <li>Capacidade de liderança</li>
+              <li>Persuasão</li>
+              <li>Trabalho em Equipe</li>
+              <li>Visão do negócio</li>
+            </ul>
           </a>
 
           <a
             href="#" className={styles.card}>
             <h3>Formação &rarr;</h3>
-            <p>...</p>
+            <ul>
+              <li>Doutorado em Materiais para Engenharia, UNIFEI, MG</li>
+              <li>Mestrado em Materiais para Engenharia, UNIFEI, MG</li>
+              <li>Bacharelado em Física, IFUSP, USP, SP</li>
+              <li>Licenciatura em Física, IFUSP, USP, SP</li>
+            </ul>
           </a>
 
           <a
             href="#" className={styles.card}>
-            <h3>Projetos Atuais &rarr;</h3>
-            <p>...</p>
+            <h3>Contato &rarr;</h3>
+            <p>alexandre.lanza@gmail.com</p>
+            <p>whatsapp: (19) 99660-5104</p>
+            <p>Campinas - SP</p>
           </a>
         </div>
       </main>
 
       <footer className={styles.footer}>
-        <p>PS: Ainda atualizando, me desculpe!</p>     
+        <p>PS: esta é a primeira versão (01/21), feita com NextJS, ReactJS e hospedada na Vercel através do GitHub.</p>     
       </footer>
     </div>
   )

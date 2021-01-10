967a6eee941ea6768178bf0303bf741170e6333a
velhogeek <alexandre.lanza@gmail.com>
2021-01-09 21:23:57 -0300

    Simples

--- pages/index.js
+++ pages/index.js
@@ -15,50 +15,47 @@ export default function Home() {
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
+          História Profissional
         </p>
 
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
+          <a href="#" className={styles.card}>
+            <h3>Interesses &rarr;</h3>
+            <p>...</p>
           </a>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
+          <a href="#" className={styles.card}>
+            <h3>Habilidades &rarr;</h3>
             <p>Learn about Next.js in an interactive course with quizzes!</p>
           </a>
 
           <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
+            href="#" className={styles.card}>
+            <h3>Formação &rarr;</h3>
+            <p>...</p>
           </a>
 
           <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
+            href="#" className={styles.card}>
+            <h3>Projetos Atuais &rarr;</h3>
+            <p>...</p>
           </a>
         </div>
       </main>
 
       <footer className={styles.footer}>
-        <a
+        <p>Devo agradecer</p>
+        <ul>
+          <li><a
           href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
           target="_blank"
           rel="noopener noreferrer"
         >
           Powered by{' '}
           <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
+        </a></li>
+        <li><a href="https://www.youtube.com/channel/UCU5JicSrEM5A63jkJ2QvGYw"><img src="https://yt3.ggpht.com/ytc/AAUvwnjmOwHGXldA8NPXJ4vIFtA4334qT5fspJMPoM4p=s88-c-k-c0x00ffffff-no-rj"></img></a>Filipe Deschampse seus vídeos</li>
+        </ul>        
       </footer>
     </div>
   )
